5a038aa7021cbb1680e4cdacc163cc0518a339c2
vshivtsevnikolai <101023538+vshivtsevnikolai@users.noreply.github.com>
2023-07-06 12:52:19 +0300

    Update

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,94 @@
 package jm.task.core.jdbc.dao;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        String sqlCreate = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)";
+        try {
+            Statement statementCreate = connection.createStatement();
+            statementCreate.executeUpdate(sqlCreate);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        String sqlDrop = "DROP TABLE IF EXISTS users";
+        try {
+            Statement statementDrop = connection.createStatement();
+            statementDrop.executeUpdate(sqlDrop);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sqlSave = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement statementSave = connection.prepareStatement(sqlSave);
+            statementSave.setString(1, name);
+            statementSave.setString(2, lastName);
+            statementSave.setByte(3, age);
+            statementSave.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        String sqlRemove = "DELETE FROM users WHERE id = ?";
+        try {
+            PreparedStatement statementRemove = connection.prepareStatement(sqlRemove);
+            statementRemove.setLong(1, id);
+            statementRemove.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sqlAll = "SELECT * FROM users";
+        List <User> users = new ArrayList<>();
+        try {
+            ResultSet result = connection.createStatement().executeQuery(sqlAll);
+            while (result.next()) {
+                User user = new User (result.getString("name"),
+                        result.getString("lastName"),
+                        result.getByte("age"));
+                user.setId(result.getLong("id"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        String sqlClean = "TRUNCATE TABLE users";
+        try {
+            Statement statementCreate = connection.createStatement();
+            statementCreate.executeUpdate(sqlClean);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/BDfirst";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "kbTFDydmuBn321";
+    public static Connection getConnection () {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {}
+        return connection;
+    }
 }
